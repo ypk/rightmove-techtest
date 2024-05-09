37983c41e9c516fa6be48b42abc0df4156c2d808
ypk <yprasannakumar@gmail.com>
2024-05-09 23:37:58 +0100

    Updated: - PropertyListing component to dynamically render content - App component to fetch data on mount - index to include data provider for the app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './App.scss';
 import Header from '../Header';
 import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
+import { AppContext } from "../../AppContext";
+import ErrorBoundary from '../../ErrorBoundary';
 
 const App = () => {
+    const { setData } = useContext(AppContext);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/properties`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await response.json();
+            setData(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+
     return (
         <div className="App">
             <Header />
             <main>
-                <SortAndFilter />
-                <PropertyListing />
+                <ErrorBoundary>
+                    <SortAndFilter />
+                    <PropertyListing />
+                </ErrorBoundary>
             </main>
         </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
+import { AppDataProvider } from './AppContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <AppDataProvider>
+        <App />
+    </AppDataProvider>
+);
